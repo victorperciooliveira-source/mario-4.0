303c15266c16f2f1e2876d8aac901f8cce0d65f2
victorperciooliveira-source <victor.percio.oliveira@escola.pr.gov.br>
2026-02-12 19:59:59 -0300

    mario 4.0

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ const restartButton = document.getElementById('restartButton');
 // Sons
 const jumpSound = new Audio('./mp3/wolfy_sanic-jump-15984.mp3');
 jumpSound.volume = 0.4; // volume mais baixo para o pulo
-const gameOverSound = new Audio('./mp3/mario-bros.mp3');
+const gameOverSound = new Audio('./mp3/mario_game_over_sms.mp3');
 
 // Música de fundo
-const backgroundMusic = new Audio('./mp3/super-mario-bros-music.mp3');
+const backgroundMusic = new Audio('./mp3/howling_houd_music-retro-egyptian-theme-187380.mp3');
 backgroundMusic.loop = true;
 backgroundMusic.volume = 1.0;
 
 // Som de mudança de fase
-const faseSound = new Audio('./mp3/super-mario-64-yahoo-sound.mp3');
-faseSound.volume = 1.0; // ajusta volume se precisar
+const faseSound = new Audio('./mp3/Mario Yahoo-QuickSounds.com.mp3);
+faseSound.volume = 5.0; // ajusta volume se precisar
 
 // Inicia a música após a primeira interação do usuário
 const iniciarMusica = () => {
