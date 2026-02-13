bc60105236e83a40198f7abe78817106d0975851
victorperciooliveira-source <victor.percio.oliveira@escola.pr.gov.br>
2026-02-13 19:58:44 -0300

    cc

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const clouds = document.querySelector('.clouds');
 const gameBoard = document.querySelector('.game-board');
 const restartButton = document.getElementById('restartButton');
 
-// Sons
-const jumpSound = new Audio('./mp3/advikgam-jump-up-245782.mp3');
-jumpSound.volume = 0.4; // volume mais baixo para o pulo
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    // Sons
+    const jumpSound = new Audio('./mp3/advikgam-jump-up-245782.mp3');
+    jumpSound.volume = 0.4; // volume mais baixo para o pulo
+}
 
 const gameOverSound = new Audio('./mp3/mario-bros.mp3');
 for (let index = 0; index < array.length; index++) {
